926f3008bb1299b4f6572b66b16e36174605eaca
Sathish <Sathish@DESKTOP-J7ELETM>
2020-02-27 17:02:15 +0530

    hiiii

---
+++ src/viratkohli/Sample1.java
@@ -0,0 +1,20 @@
+package viratkohli;
+
+public class Sample1 {
+	
+	
+	public int add(int x, int y)
+	{
+		
+		int z=x+y;
+		return(z);
+	}
+	public int sub(int x,int y)
+	{
+		int z;
+		z=x-y;
+		return(z);
+	}
+
+
+}
---
+++ src/viratkohli/Sample2.java
@@ -0,0 +1,15 @@
+package viratkohli;
+
+public class Sample2 extends Sample1
+{
+	
+
+	public int add(int x, int y)
+	{
+		
+		int z=x-y;
+		return(z);
+
+}
+	
+}
